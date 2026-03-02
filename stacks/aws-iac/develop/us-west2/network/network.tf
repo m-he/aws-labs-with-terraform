34728ca5e0071c9a1187e6215cb905c782417766
Hong E <engineer@zerone.is>
2026-03-02 02:57:58 +0000

    refactor network

--- stacks/aws-iac/develop/us-west2/network/network.tf
+++ stacks/aws-iac/develop/us-west2/network/network.tf
@@ -1,60 +1,36 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = {
-    Name = "${local.env}-main"
-  }
+  tags = merge(local.common_tags, {
+    Name = "${local.cluster_name}-vpc"
+  })
 }
 
-resource "aws_subnet" "private_zone_1" {
+resource "aws_subnet" "private" {
+  for_each          = local.az_map
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/19"
-  availability_zone = local.zone1
+  availability_zone = each.key
+  cidr_block        = cidrsubnet(var.vpc_cidr, 2, each.value)
 
-  tags = {
-    "Name"                                                 = "${local.env}-private-${local.zone1}"
-    "kubernetes.io/role/internal-elb"                      = "1"
-    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
-  }
+  tags = merge(local.common_tags, {
+    Name                                          = "${local.cluster_name}-private-${each.key}"
+    "kubernetes.io/role/internal-elb"             = "1"
+    "kubernetes.io/cluster/${local.cluster_name}" = var.cluster_tag_value
+  })
 }
 
-resource "aws_subnet" "private_zone_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.32.0/19"
-  availability_zone = local.zone2
-
-  tags = {
-    "Name"                                                 = "${local.env}-private-${local.zone2}"
-    "kubernetes.io/role/internal-elb"                      = "1"
-    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
-  }
-}
-
-resource "aws_subnet" "public_zone_1" {
+resource "aws_subnet" "public" {
+  for_each                = local.az_map
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.64.0/19"
-  availability_zone       = local.zone1
+  availability_zone       = each.key
+  cidr_block              = cidrsubnet(var.vpc_cidr, 2, each.value + 2)
   map_public_ip_on_launch = true
 
-  tags = {
-    "Name"                                                 = "${local.env}-public-${local.zone1}"
-    "kubernetes.io/role/elb"                               = "1"
-    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
-  }
-}
-
-resource "aws_subnet" "public_zone_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.96.0/19"
-  availability_zone       = local.zone2
-  map_public_ip_on_launch = true
-
-  tags = {
-    "Name"                                                 = "${local.env}-public-${local.zone2}"
-    "kubernetes.io/role/elb"                               = "1"
-    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
-  }
+  tags = merge(local.common_tags, {
+    Name                                          = "${local.cluster_name}-public-${each.key}"
+    "kubernetes.io/role/elb"                      = "1"
+    "kubernetes.io/cluster/${local.cluster_name}" = var.cluster_tag_value
+  })
 }
--- stacks/aws-iac/develop/us-west2/network/variables.tf
+++ stacks/aws-iac/develop/us-west2/network/variables.tf
@@ -1,7 +1,32 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 locals {
-  env      = "staging"
-  region   = "us-west-2"
-  zone1    = "us-west-2a"
-  zone2    = "us-west-2b"
-  eks_name = "demo"
+  region       = "us-west-2"
+  cluster_name = "${var.env}-${var.eks_name}"
+  azs          = slice(data.aws_availability_zones.available.names, 0, 2)
+  az_map       = { for idx, az in local.azs : az => idx }
+
+  common_tags = merge(var.tags, {
+    Environment = var.env
+    Terraform   = "true"
+  })
+}
+
+variable "env" { type = string }
+variable "eks_name" { type = string }
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "cluster_tag_value" {
+  type    = string
+  default = "owned"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
